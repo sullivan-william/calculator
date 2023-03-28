4350bb500afdac2113888a035a978059bf7a9495
William Sullivan <sullivanwp95@gmail.com>
2023-03-28 18:12:10 -0400

    Function to highlight currently selected operator

--- main.js
+++ main.js
@@ -9,13 +9,22 @@ let operator;
 let y = ''; // second operand
 let answer = 0;
 
+let selectedOperator;
+function highlightSelectedOperator() {
+    operators.forEach(operator => {
+        if (operator.style.backgroundColor === 'aqua' && operator !== selectedOperator) {
+            operator.style.backgroundColor = 'orange'
+        }
+    })
+}
+
 operands.map(operand => {
     operand.addEventListener('click', (e) => {
         if (!operator || x === '') {
             operator = null // don't allow operator to be selected without first operand(x)
             x += e.target.textContent
             result.textContent = x // change display to match first operand(x)
-        } else { // if there is an operator and a first operand(x) selected move on to second operand(y)
+        } else {
             y += e.target.textContent
             result.textContent = y
         }
@@ -24,37 +33,47 @@ operands.map(operand => {
 
 operators.map(button => {
     button.addEventListener('click', (e) => {
-        operator = e.target.textContent
-        y = '' // resets y if one operation has already occurred
+        if (x !== '') {
+            operator = e.target.textContent
+            y = '' // resets y if starting a new operation with current answer as the first operand(x)
+            selectedOperator = e.target
+            selectedOperator.style.backgroundColor = 'aqua'
+            highlightSelectedOperator()
+        }
     }) 
 });
 
 equal.addEventListener('click', (e) => {
-    x = Number(x)
-    y = Number(y)
-    
-    switch (operator) {
-        case '+':
-            add(x, y)
-            result.textContent = answer
-            x = answer
-            break;
-        case '-':
-            sub(x, y)
-            result.textContent = answer
-            x = answer
-            break;
-        case 'x':
-            mult(x, y)
-            result.textContent = answer
-            x = answer
-            break;
-        case '/':
-            divide(x, y)
-            result.textContent = answer
-            x = answer
-            break;
+    if (x === '' || y === '') {
+        result.textContent = x // if equal button pressed with one or less operands, return single operand or empty string
+    } else { // otherwise, turn x and y into numbers and run function based on the operator
+        x = Number(x)
+        y = Number(y)
+
+        switch (operator) {
+            case '+':
+                add(x, y)
+                result.textContent = answer
+                x = answer
+                break;
+            case '-':
+                sub(x, y)
+                result.textContent = answer
+                x = answer
+                break;
+            case 'x':
+                mult(x, y)
+                result.textContent = answer
+                x = answer
+                break;
+            case '/':
+                divide(x, y)
+                result.textContent = answer
+                x = answer
+                break;
+        }
     }
+    
 });
 
 clear.addEventListener('click', () => {
@@ -63,6 +82,8 @@ clear.addEventListener('click', () => {
     y = ''
     answer = 0
     result.textContent = ''
+    selectedOperator = null
+    highlightSelectedOperator()
 });
 
 function add(x, y) {
